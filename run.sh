1e32fd6a2911c95ab1bde47d5158a21d636b6622
Riccardo Serraino <167330242+riccardoserraino@users.noreply.github.com>
2026-04-01 14:05:05 +0200

    Update run.sh to include environment variable settings
    Added environment variable exports and updated Python script execution.

--- run.sh
+++ run.sh
@@ -2,5 +2,14 @@
 set -e
 
 source venv/bin/activate
+
+# Uncomment the following line if new pkg are needed
 pip install -r requirements.txt
-python src/main.py
+
+# to fix some terminal warnings related to tf
+export TF_CPP_MIN_LOG_LEVEL=2 
+export TF_ENABLE_ONEDNN_OPTS=0
+export CUDA_VISIBLE_DEVICES=""
+
+
+python3 src/papi_main.py
